dbddcb070d11dd29fef75fb9765f84c622286cbb
sinthushan <sinthushan@gmail.com>
2021-01-09 22:12:22 -0500

    added code to determine when change in format occured

--- Edgar_Scrape.py
+++ Edgar_Scrape.py
@@ -1,18 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
+#import pandas as pd
 
 filing_list_url = "https://www.sec.gov/cgi-bin/browse-edgar"
 
 payload = {
         'action': 'getcompany', 
-        'CIK': '51143',
+        'CIK': '320193',
         'type': '10-K',
         'count': '200'
     }
+
 r = requests.get(filing_list_url, params=payload)
 
-
 soup = BeautifulSoup(r.text, 'html.parser')
-for link in soup.find_all(id="interactiveDataBtn"):
-    print(link.get('href'))
+
+account_numbers = [link.get('href')
+                    .split('accession_number=')[1]
+                    .split('&')[0]
+                    .replace('-','') 
+                    for link in soup.find_all(id="interactiveDataBtn")]
+
+base_url = 'https://www.sec.gov/Archives/edgar/data/'
+
+for account in account_numbers:
+    url = base_url + payload['CIK'] + f"/{account}"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    links = soup.select('a[href*="R"][href$=".htm"]')
+    if links:
+        for link in links:
+            
+    else:
+        break
+        #url = base_url + payload['CIK'] + f"/{account}/R{index}.htm" 
+   
+
 
