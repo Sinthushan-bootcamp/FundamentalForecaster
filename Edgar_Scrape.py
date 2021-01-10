8cc752acfc19384cdf9cdc004c92ab4105614773
sinthushan <sinthushan@gmail.com>
2021-01-10 15:28:02 -0500

    completed balancesheet to json

--- Edgar_Scrape.py
+++ Edgar_Scrape.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-#import pandas as pd
+import pandas as pd
+import json
+import math
+from re import sub
+from decimal import Decimal
 
 filing_list_url = "https://www.sec.gov/cgi-bin/browse-edgar"
 
@@ -15,25 +19,44 @@ r = requests.get(filing_list_url, params=payload)
 
 soup = BeautifulSoup(r.text, 'html.parser')
 
-account_numbers = [link.get('href')
-                    .split('accession_number=')[1]
-                    .split('&')[0]
-                    .replace('-','') 
-                    for link in soup.find_all(id="interactiveDataBtn")]
+balance_sheets = []
+for link in soup.find_all(id="interactiveDataBtn"):
+    r = requests.get('https://www.sec.gov/' + link.get('href'))
+    statement_soup = BeautifulSoup(r.text, 'html.parser')
+    balance_sheet_link = statement_soup.find_all("a", string="CONSOLIDATED BALANCE SHEETS")
+    if balance_sheet_link:
+        balance_sheets.append({
+            'account_number':link.get('href')
+                                .split('accession_number=')[1]
+                                .split('&')[0]
+                                .replace('-',''),
+            'index':balance_sheet_link[0]['href']
+                                .split('(')[1]
+                                .split(')')[0]
+        }
+        )
 
 base_url = 'https://www.sec.gov/Archives/edgar/data/'
+json_output = json.loads('{}')
+for balance_sheet in balance_sheets:
+    url = base_url + payload['CIK'] + f"/{balance_sheet['account_number']}/R{balance_sheet['index']}.htm"
+    try:
+        df = pd.read_html(url)[0]
+        df[df.columns[0]] = df[df.columns[0]].replace(
+                                to_replace=r'[^a-zA-Z\s]',
+                                value='', 
+                                regex=True
+                            ).replace(r'\r\n', '').replace(r'\n', '').replace(r'\r', '')
+        balance_sheet_groups = []
+        for index, row in df.iterrows():
+            if type(row[df.columns[1]]) == float:
+                balance_sheet_group = row[df.columns[0]]
+            balance_sheet_groups.append(balance_sheet_group)
+        df['balance_sheet_group'] = balance_sheet_groups
+        df = df[df[df.columns[0]] != balance_sheet_groups ]
+        df = df.set_index(['balance_sheet_group', df.columns[0]])
+        json_output.update(json.loads(df.to_json()))
+    except:
+        pass
 
-for account in account_numbers:
-    url = base_url + payload['CIK'] + f"/{account}"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    links = soup.select('a[href*="R"][href$=".htm"]')
-    if links:
-        for link in links:
-            
-    else:
-        break
-        #url = base_url + payload['CIK'] + f"/{account}/R{index}.htm" 
-   
-
-
+print(json_output)
